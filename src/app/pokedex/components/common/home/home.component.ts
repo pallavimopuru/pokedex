1d0d01d9b7cdad3de07dffb83930b87398a1d50a
Pallavi Mopuru <mopurupallavi@gmail.com>
2023-10-29 15:34:49 +0530

    search implemented

--- src/app/pokedex/components/common/home/home.component.ts
+++ src/app/pokedex/components/common/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { PokedexService } from 'src/app/shared/service/pokedex.service';
 
 @Component({
@@ -12,6 +13,7 @@ export class HomeComponent {
   pokemon: any = [];
   spriteUrl: string = '';
   currentPage: number = 1;
+  filteredPokemonDetails: any[] = [];
 
   first: number = 0;
 
@@ -19,13 +21,15 @@ export class HomeComponent {
   search: any;
   searchText: any;
 
-  constructor(private pokedexService: PokedexService) {}
+  constructor(private pokedexService: PokedexService,private router:Router) {}
 
   ngOnInit() {
     this.pokedexService.getpokemans().subscribe((res: any) => {
       this.pokemon = res.results; // Get the first Pokémon for demonstration
       // console.log(this.pokemon);
       this.getAllFormDetails(); // Assuming you have an "id" property in your Pokémon data
+      this.applySearchFilter();
+   
     });
   }
 
@@ -34,32 +38,54 @@ export class HomeComponent {
       // console.log(element);
 
       this.pokedexService
-        .getPokemonDetailsAbility(element?.url)
-        .subscribe((ability: any) => {
-          //  console.log(ability);
-          if (ability) {
-            this.pokedexService
-              .getPokemonDetailsForms(ability?.forms[0]?.url)
-              .subscribe((formDetails: any) => {
-                console.log(formDetails);
-                if (formDetails) {
-                  let obj = {
-                    pokemonName: formDetails.pokemon.name,
-                    pokemonImg: formDetails.sprites.back_default,
-                  };
-                  this.pokemonDetails.push(obj);
-                }
-              });
-          }
-        });
+      .getPokemonDetailsAbility(element?.url)
+      .subscribe((ability: any) => {
+        if (ability) {
+          const height = ability.height; // Access the "height" property
+          const id=ability.id;
+          this.pokedexService
+            .getPokemonDetailsForms(ability?.forms[0]?.url)
+            .subscribe((formDetails: any) => {
+              if (formDetails) {
+                let obj = {
+                  pokemonName: formDetails.pokemon.name,
+                  pokemonImg: formDetails.sprites.back_default,
+                  pokemonHeight: height, // Include the "height" property in your object
+                  pokemonId:id
+                };
+                this.pokemonDetails.push(obj);
+              }
+            });
+        }
+      });
+ 
+    
+    
+    
     });
   }
-  get filteredPokemon() {
-    return this.pokemonDetails.filter((pokemon:any) =>
-      pokemon.pokemonName.toLowerCase().includes(this.searchText.toLowerCase())
-    );
+  applySearchFilter() {
+    if (this.search) {
+      this.filteredPokemonDetails = this.pokemonDetails.filter((pokemon:any) => {
+        // Convert both the name and ID to lowercase for a case-insensitive search
+        const searchValue = this.search.toLowerCase();
+        const pokemonName = pokemon.pokemonName.toLowerCase();
+        const pokemonId = pokemon.pokemonId.toString(); // Convert ID to a string
+  
+        // Check if the search value is present in either the name or ID
+        return pokemonName.includes(searchValue) || pokemonId.includes(searchValue);
+      });
+    } else {
+      // If the search input is empty, show all Pokémon
+      this.filteredPokemonDetails = this.pokemonDetails;
+    }
   }
 
+  onclickimg(){
+    this.router.navigateByUrl('/pokedex');
+    // After setting details in pokemonDetails
+
+  }
 
   onPageChange(event: any) {
     this.first = event.first;
--- src/app/pokedex/components/common/pokedex/pokedex.component.ts
+++ src/app/pokedex/components/common/pokedex/pokedex.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokedex.component.scss']
 })
 export class PokedexComponent {
+  storedData:any;
+constructor(){
+
+}
+ngOnInit(){
+
+}
 
 }
